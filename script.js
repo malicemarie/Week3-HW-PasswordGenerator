f248a513b2d06b0073b55e2e19fa528d1d919c7f
Marie Lenac <marielenac@iPhonediaRebeca.localdomain>
2019-12-11 14:41:00 -0800

    for loop

--- script.js
+++ script.js
@@ -1,34 +1,56 @@
-// var numOfChar = prompt('How many Characters do you want in your password?');
-// var specialChar = confirm("Do you want to use Special Characters?");
-// var lowerCase = confirm("Do you want to use lower case characters?");
-// var upperCase = confirm("Do you want to use upper case characters?");
-// var includeNum = confirm("Do you want to include numbers?");
+var numOfChar = prompt('How many Characters do you want in your password?')
 
-// console.log({
-//     numOfChar, 
-//     specialChar, 
-//     lowerCase, 
-//     upperCase, 
-//     includeNum
-// })
+for (var i = numOfChar; i <= 8 || i >= 128; numOfChar = prompt(
+    'How many Characters do you want in your password?')) {
+    console.log(i);
+
+    if (numOfChar < 8) {
+        alert("Not enough Characters");
+    }
+    else if (numOfChar > 128) {
+        alert("Too many characters");
+    }
+
+    else if (numOfChar >= 8 && numOfChar <= 128) {
+        alert("sounds good!");
+        break
+    }
+
+}
+
+
+
+
+
+
+// run this shit until they answer correctly
+
+
+
+var specialChar = confirm("Do you want to use Special Characters?");
+var lowerCase = confirm("Do you want to use lower case characters?");
+var upperCase = confirm("Do you want to use upper case characters?");
+var includeNum = confirm("Do you want to include numbers?");
+
+console.log({
+    numOfChar,
+    specialChar,
+    lowerCase,
+    upperCase,
+    includeNum
+})
 
 var approvedChar = 'abcdefghijklmnopqrstuvwxyz'.split("");
 console.log(approvedChar);
-
-
-
-// if (numOfChar < 8) {
-//     alert("Not enough Characters");
-// }
-// if (numOfChar > 128) {
-//     alert("Too many characters");
-// }
-// else {
-//     alert("Sounds good!");
-// }
+var approvedSpecChar = ['!', '@', '#', '$', '%', "?", "&", "*", "+"];
+console.log(approvedSpecChar);
 
 
 
 
-// var specialCharacters = ['!', '@', '#', '$', '%', "?", "&", "*", "+"]
+
+
+
+
+var specialCharacters = ['!', '@', '#', '$', '%', "?", "&", "*", "+"]
 
