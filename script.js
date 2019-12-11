52cf1150712b45cefc4db26503d5f8691b47b5ce
Marie Lenac <marielenac@iPhonediaRebeca.localdomain>
2019-12-11 13:45:46 -0800

    initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+var numOfChar = prompt('How many Characters do you want in your password?');
+var specialChar = confirm("Do you want to use Special Characters?");
+var lowerCase = confirm("Do you want to use lower case characters?");
+var upperCase = confirm("Do you want to use upper case characters?");
+var includeNum = confirm("Do you want to include numbers?");
+
+console.log({
+    numOfChar, 
+    specialChar, 
+    lowerCase, 
+    upperCase, 
+    includeNum
+})
+
+
+
+if (numOfChar < 8) {
+    alert("Not enough Characters");
+}
+if (numOfChar > 128) {
+    alert("Too many characters");
+}
+else {
+    alert("Sounds good!");
+}
+
+
+
+
+var specialCharacters = ['!', '@', '#', '$', '%', "?", "&", "*", "+"]
+
