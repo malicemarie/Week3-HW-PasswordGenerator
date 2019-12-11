6aa15ceb227ed2a1a455b8647de2681dd9363d97
Marie Lenac <marielenac@iPhonediaRebeca.localdomain>
2019-12-11 14:04:30 -0800

    array of approved characters

--- script.js
+++ script.js
@@ -1,31 +1,34 @@
-var numOfChar = prompt('How many Characters do you want in your password?');
-var specialChar = confirm("Do you want to use Special Characters?");
-var lowerCase = confirm("Do you want to use lower case characters?");
-var upperCase = confirm("Do you want to use upper case characters?");
-var includeNum = confirm("Do you want to include numbers?");
+// var numOfChar = prompt('How many Characters do you want in your password?');
+// var specialChar = confirm("Do you want to use Special Characters?");
+// var lowerCase = confirm("Do you want to use lower case characters?");
+// var upperCase = confirm("Do you want to use upper case characters?");
+// var includeNum = confirm("Do you want to include numbers?");
 
-console.log({
-    numOfChar, 
-    specialChar, 
-    lowerCase, 
-    upperCase, 
-    includeNum
-})
+// console.log({
+//     numOfChar, 
+//     specialChar, 
+//     lowerCase, 
+//     upperCase, 
+//     includeNum
+// })
 
-
-
-if (numOfChar < 8) {
-    alert("Not enough Characters");
-}
-if (numOfChar > 128) {
-    alert("Too many characters");
-}
-else {
-    alert("Sounds good!");
-}
+var approvedChar = 'abcdefghijklmnopqrstuvwxyz'.split("");
+console.log(approvedChar);
 
 
 
+// if (numOfChar < 8) {
+//     alert("Not enough Characters");
+// }
+// if (numOfChar > 128) {
+//     alert("Too many characters");
+// }
+// else {
+//     alert("Sounds good!");
+// }
 
-var specialCharacters = ['!', '@', '#', '$', '%', "?", "&", "*", "+"]
+
+
+
+// var specialCharacters = ['!', '@', '#', '$', '%', "?", "&", "*", "+"]
 
